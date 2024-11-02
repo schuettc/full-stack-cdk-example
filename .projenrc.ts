ccc145fb237fedc76049d04cae69e19aa7eebce3
Court Schuett <schuettc@amazon.com>
2024-11-01 23:18:15 -0500

    fix: update deps

---
+++ .projenrc.ts
@@ -0,0 +1,40 @@
+const { awscdk } = require('projen');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  author: 'Court Schuett',
+  projenrcTs: true,
+  authorAddress: 'https://subaud.io',
+  jest: false,
+  cdkVersion: '2.31.0',
+  license: 'MIT-0',
+  copyrightOwner: 'Court Schuett',
+  defaultReleaseBranch: 'main',
+  name: 'full-stack-cdk-example',
+  deps: ['fs-extra', '@types/fs-extra'],
+  devDeps: ['@types/prettier@2.6.0', 'esbuild', 'got@11.8.5', 'ts-node@^10'],
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['schuettc'],
+  },
+  workflowNodeVersion: '16.x',
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  repositoryUrl: 'https://github.com/schuettc/full-stack-cdk-example',
+});
+
+const common_exclude = [
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  'dependabot.yml',
+  '*.drawio',
+];
+
+project.gitignore.exclude(...common_exclude);
+project.synth();
--- test/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { App } from 'aws-cdk-lib';
-import { FullStack } from '../src/main';
-
-test('Snapshot', () => {
-  const app = new App();
-  new FullStack(app, 'test');
-});
